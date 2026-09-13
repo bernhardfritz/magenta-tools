c19de334ed565f90f19301c4208d4aedd8857d00
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2026-09-13 20:33:51 +0200

    feat: update script to work with newly added encryption mechanism

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import { CookieJar } from 'tough-cookie';
-import { createHash } from 'crypto';
+import { createHash, createCipheriv } from 'crypto';
 import 'dotenv/config';
 import _ from 'lodash';
+
+async function encryptPassword(sessionToken, password) {
+    // 1. Generate key and IV using SHA256 and MD5 hashes
+    const key = createHash('sha256').update(sessionToken).digest();
+    const iv = createHash('md5').update(sessionToken).digest();
+
+    // 2. Encrypt using AES-256-CBC with PKCS7 padding (Node's default padding)
+    const cipher = createCipheriv('aes-256-cbc', key, iv);
+    let ciphertext = cipher.update(password, 'utf8', 'base64');
+    ciphertext += cipher.final('base64');
+
+    // 3. Replicate the original double-Base64 logic:
+    // Convert base64 ciphertext back to a Hex string (matching CryptoJS.enc.Base64.parse().toString())
+    const hexString = Buffer.from(ciphertext, 'base64').toString('hex');
+
+    // Prepend a colon and encode the final string to Base64
+    const finalResult = Buffer.from(":" + hexString).toString('base64');
+
+    return finalResult;
+}
 
 if (!process.env.PASSWORD) {
   throw new Error('Environment variable PASSWORD is not set.');
 }
 
-const hashedPassword = createHash('sha256')
-  .update(process.env.PASSWORD)
-  .digest('hex');
 const host = 'http://192.168.0.1';
 const cookieJar = new CookieJar();
 
@@ -17,6 +34,11 @@ console.log('Retrieving token...');
 let res = await fetch(`${host}/`);
 await cookieJar.setCookie(res.headers.get('set-cookie'), `${host}/`);
 console.log('Token retrieved successfully!');
+
+const sessionToken = _.find(await cookieJar.getCookies(`${host}/`), {
+    key: 'sessionToken',
+  }).value;
+const encryptedPassword = encryptPassword(sessionToken, process.env.PASSWORD);
 
 console.log('Logging in...');
 res = await fetch(`${host}/xml/setter.xml`, {
@@ -26,12 +48,10 @@ res = await fetch(`${host}/xml/setter.xml`, {
     Cookie: await cookieJar.getCookieString(`${host}/`),
   },
   body: new URLSearchParams({
-    token: _.find(await cookieJar.getCookies(`${host}/`), {
-      key: 'sessionToken',
-    }).value,
+    token: sessionToken,
     fun: 15,
     Username: 'NULL',
-    Password: hashedPassword,
+    Password: encryptedPassword,
   }),
 });
 await cookieJar.setCookie(res.headers.get('set-cookie'), `${host}/`);
